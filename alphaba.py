b00f8dbda07089f95ec1b00c27099c4c26d0faa0
Vipvs <36496105+Vipvs@users.noreply.github.com>
2018-02-15 14:00:12 +0530

    Update and rename alphabt.py to alphaba.py

--- alphaba.py
+++ alphaba.py
@@ -5,6 +5,6 @@ while True:
 		break
 	else:
 		if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-			print(ch, "is an alphabet.\n")
+			print(ch,"is an alphabet.\n")
 		else:
-			print(ch, "is not an alphabet.\n")
+			print(ch,"is not an alphabet.\n")
