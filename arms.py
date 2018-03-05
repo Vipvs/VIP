f73656f1960e13c2e2bf7d1ed315e3ad20324e2d
Vipvs <36496105+Vipvs@users.noreply.github.com>
2018-03-05 04:19:27 -0800

    Update arms.py

--- arms.py
+++ arms.py
@@ -2,6 +2,6 @@ n=int(input("Enter any number: "))
 a=list(map(int,str(n)))
 b=list(map(lambda x:x**3,a))
 if(sum(b)==n):
-    print("The number is an armstrong number. ")
+    print("yes ")
 else:
-    print("The number isn't an arsmtrong number. ")
+    print("no ")
