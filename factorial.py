aa09d8ce523eb4d9a70d5fb7ec94f95907dbe324
Vipvs <36496105+Vipvs@users.noreply.github.com>
2018-03-05 21:24:25 +0530

    Update and rename fact.py to factorial.py

--- factorial.py
+++ factorial.py
@@ -6,5 +6,5 @@ elif num == 0:
     print("The factorial of 0 is 1")
 else:
     for i in range(1, num + 1):
-        factorial *= i
+       factorial *= i
     print("The factorial of", num, "is", factorial)
