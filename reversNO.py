bb8e2a88a7f9350570288e620037597a0f7fa345
Vipvs <36496105+Vipvs@users.noreply.github.com>
2018-03-06 03:51:58 -0800

    Update reversNO.py

--- reversNO.py
+++ reversNO.py
@@ -3,5 +3,5 @@ Reverse = 0
 while(Number > 0):
     Reminder = Number %10
     Reverse = (Reverse *10) + Reminder
-    Number = Number //10
- print("\n Reverse of entered number is = %d" %Reverse)
+     Number = Number //10 
+print("\n Reverse of entered number is = %d" %Reverse)
